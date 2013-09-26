a71ebb86423373e0e1764a33bccb3c11275dbba6
Chris Haley <chris.haley@meltmedia.com>
2013-09-25 22:57:50 -0700

    hopefully this doesn't break anything

--- unity/Assets/Scripts/NetworkManager.cs
+++ unity/Assets/Scripts/NetworkManager.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 	
-	private string gameName = "melt nerf wars game";
+	public string gameName = "melt nerf wars game";
+	public GameObject playerPrefab;
+	public Transform spawnObject;
+	
 	private float btnX;
 	private float btnY;
 	private float btnW;
 	private float btnH;
+	private bool refreshing = false;
+	private HostData[] hostdata;
+	
+
 	// Use this for initialization
 	void Start () {
 		btnX = Screen.width * 0.05f;
@@ -16,15 +23,40 @@ public class NetworkManager : MonoBehaviour {
 		btnH = Screen.width * 0.05f;
 	}
 	
+	void Update() {
+		if(refreshing) {
+			if(MasterServer.PollHostList().Length > 0) {
+				refreshing = false;
+				Debug.Log (MasterServer.PollHostList().Length);
+				hostdata = MasterServer.PollHostList();
+				
+			}
+		}
+	}
 	
 	void startServer() {
-		Network.InitializeServer(32,25001,Network.HavePublicAddress());
+		Network.InitializeServer(32,25001,!Network.HavePublicAddress());
 		MasterServer.RegisterHost(gameName,"melt nerf wars","Melt nerf name!");
 		
 	}
 	
+	void refreshHostList() {
+		MasterServer.RequestHostList(gameName);
+		refreshing = true;
+		
+	}
+	
+	void spawnPlayer() {
+		Network.Instantiate(playerPrefab,spawnObject.position,Quaternion.identity,0);
+	}
+	
 	void OnServerInitialized() {
 		Debug.Log ("Server Initialized");
+		spawnPlayer();
+	}
+	
+	void OnConnectedToServer() {
+		spawnPlayer();
 	}
 	
 	void OnMasterServerEvent(MasterServerEvent mse) {
@@ -33,14 +65,32 @@ public class NetworkManager : MonoBehaviour {
 		}
 	}
 	
+	 IEnumerator wait(int timeToWait) {
+        yield return new WaitForSeconds(timeToWait);
+    }
+	
+	
+	// GUI
 	void OnGUI () {
-		if(GUI.Button(new Rect(btnX,btnY,btnX,btnH), "Start Server")) {
-			Debug.Log ("Starting Server");
-			startServer();
+		if(!Network.isClient && !Network.isServer) {
+			if(GUI.Button(new Rect(btnX,btnY,btnW,btnH), "Start Server")) {
+				Debug.Log ("Starting Server");
+				startServer();
+			}
+			
+			if(GUI.Button(new Rect(btnX,btnY * 1.2f + btnH,btnX,btnH), "Refresh Hosts")) {
+				Debug.Log ("Refreshing");
+				refreshHostList();
+			}
+			if(hostdata != null) {
+				for(int i = 0; i<hostdata.Length;i++) {
+					if(GUI.Button(new Rect(btnX * 1.5f + btnW,btnY*1.2f + (btnH * i),btnW*3f,btnH*.5f), hostdata[i].gameName)) {
+						Network.Connect(hostdata[i]);
+					}
+				}
+			}
 		}
-		
-		if(GUI.Button(new Rect(btnX,btnY * 1.2f + btnH,btnX,btnH), "Refresh Hosts")) {
-			Debug.Log ("Refreshing");
-		}
+			
 	}
 }
+
