fd8f0d90cbbff33a7f4d56348c5faf52ebc08850
Chris Haley <chris.haley@meltmedia.com>
2013-10-08 08:53:57 -0700

    added needed assets

---
+++ unity/Assets/GUICrosshair.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUICrosshair : MonoBehaviour {
+	
+	public Texture2D crosshairTexture;
+	private Rect position;
+	private static bool originalOn = true;
+
+	// Use this for initialization
+	void Start () {
+	position = new Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - 
+        crosshairTexture.height) /2, crosshairTexture.width, crosshairTexture.height);
+	}
+	
+	void OnGUI () {
+		if(originalOn == true)
+	    {
+	        GUI.DrawTexture(position, crosshairTexture);
+	    }
+	}
+}
+
+   
+ 
