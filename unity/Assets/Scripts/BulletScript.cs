e476edc202d9ed9a7caa76be3d17efa41df31e39
Chris Haley <chris.haley@meltmedia.com>
2013-10-16 09:17:28 -0700

    just and update don't mind me

---
+++ unity/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	
+	public GameObject fireEffect;
+	float lifeSpan = 3.0f;
+	Vector3 lastVelocity;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate() {
+    lastVelocity = gameObject.rigidbody.velocity;
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "Table") {
+			Vector3 location = collision.transform.position;
+			location.y = 3;
+			Instantiate(fireEffect, location, Quaternion.identity);
+		}
+		
+		if(collision.gameObject.tag == "Monitor") {
+			Debug.Log("Hi");
+			ContactPoint contact = collision.contacts[0];
+			gameObject.rigidbody.isKinematic = true;
+			transform.position = contact.point - lastVelocity.normalized;
+		}
+		
+	}
+
+}
