189f1c1fbdd5b8912af4752dbe322e7e9041008d
Chris Haley <chris.haley@meltmedia.com>
2013-10-20 16:32:38 -0700

    updated bullet script to remove un used items

--- unity/Assets/Scripts/BulletScript.cs
+++ unity/Assets/Scripts/BulletScript.cs
@@ -5,7 +5,6 @@ public class BulletScript : MonoBehaviour {
 	
 	public GameObject fireEffect;
 	float lifeSpan = 5;
-	Vector3 lastVelocity;
 	
 	// Use this for initialization
 	void Start () {
@@ -19,17 +18,9 @@ public class BulletScript : MonoBehaviour {
 		}
 	}
 	
-	void FixedUpdate() {
-    lastVelocity = gameObject.rigidbody.velocity;
-	}
 	
 	// Update is called once per frame
 	void OnCollisionEnter(Collision collision) {
-		if(collision.gameObject.tag == "Table") {
-			Vector3 location = collision.transform.position;
-			location.y = 3;
-			Instantiate(fireEffect, location, Quaternion.identity);
-		}
 		
 		if(collision.gameObject.tag == "Monitor") {
 			Debug.Log("Hi");
