3bbff99b4108eef0b6e92a1fb550af4365fceb5b
Chris Haley <chris.haley@meltmedia.com>
2013-10-24 18:32:02 -0700

    added basic enemy... it's fun shoot him 5 times he dies

--- unity/Assets/Scripts/BulletScript.cs
+++ unity/Assets/Scripts/BulletScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class BulletScript : MonoBehaviour {
 	
-	public GameObject fireEffect;
 	float lifeSpan = 5;
 	
 	// Use this for initialization
@@ -21,11 +20,19 @@ public class BulletScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void OnCollisionEnter(Collision collision) {
+		Debug.Log("Hit something");
 		
 		if(collision.gameObject.tag == "Monitor") {
 			Debug.Log("Hi");
 			var stickyJoint = gameObject.AddComponent<FixedJoint>();
 			stickyJoint.connectedBody = collision.rigidbody;
+		}
+		
+		if(collision.gameObject.tag == "Enemy" ) {
+			collision.gameObject.GetComponent<Enemy>().health -= 1;
+			if(collision.gameObject.GetComponent<Enemy>().health <= 0) {
+				Destroy(collision.gameObject);
+			}
 		}
 		
 	}
