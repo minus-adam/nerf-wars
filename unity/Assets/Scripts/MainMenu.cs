7aa17b235f081dedb4ef4deeb9d2e8f3496ca97e
Chris Haley <chris.haley@meltmedia.com>
2013-10-29 17:22:28 -0700

    added main menu... loads scene and passes through game type... May revisit how game type works fixes #3

---
+++ unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	public bool isSingle = false;
+	public bool isCo = false;
+	public bool isMultiPlayer = false;
+	public bool isQuit = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+	
+	void OnMouseExit() {
+	renderer.material.color = Color.white;
+	}
+	
+	void OnMouseUp() {
+		if (isQuit) {
+			Application.Quit();
+		}
+		
+		if (isSingle) {
+			Globals.isSingle = true;
+			Globals.isCo = false;
+			Globals.isMulti = false;
+			Application.LoadLevel(1);
+		}
+		
+		if (isCo) {
+			Globals.isSingle = false;
+			Globals.isCo = true;
+			Globals.isMulti = false;
+			Application.LoadLevel(1);
+		}
+		
+		if (isMultiPlayer) {
+			Globals.isSingle = false;
+			Globals.isCo = false;
+			Globals.isMulti = true;
+			Application.LoadLevel(1);
+		}
+	}
+}
---
+++ unity/Assets/Scripts/PrefLoading.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefLoading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Game Type is Sigle: " + Globals.isSingle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
