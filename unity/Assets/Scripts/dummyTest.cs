1cf31c249bc32affe79d683e18c5fa76f01291d9
vincentbraun <vincent.braun@meltmedia.com>
2013-11-03 02:44:05 -0700

    updated dummy FBX to use mechanim system

---
+++ unity/Assets/Scripts/dummyTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dummyTest : MonoBehaviour {
+	Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+		anim.animation.Play("walk");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
